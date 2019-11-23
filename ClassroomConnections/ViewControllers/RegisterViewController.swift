9a6be9f5ab01e7a0bc863bfb4e3c0b9d17887252
Michael Peng <mpeng893@student.fuhsd.org>
2019-11-23 14:43:34 -0800

    asf

--- ClassroomConnections/ViewControllers/RegisterViewController.swift
+++ ClassroomConnections/ViewControllers/RegisterViewController.swift
@@ -7,24 +7,50 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     //Variables
-    var isStudent : Boolean = true
+    var isStudent : Bool = true
+    @IBOutlet weak var FirstName: UITextField!
+    @IBOutlet weak var LastName: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    //Buttons
+    @IBOutlet weak var studentButton: UIButton!
+    @IBOutlet weak var TeachersButton: UIButton!
+    
     //Firebase
     var ref: DatabaseReference!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        adjustButtons()
+        
+        
+        
+        
     }
     
     
     func adjustButtons(){
-        
+        studentButton.backgroundColor = UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 214.0/255.0, alpha: 1.0)
+        TeachersButton.backgroundColor = UIColor(red: 88.0/255.0, green: 86.0/255.0, blue: 214.0/255.0, alpha: 1.0)
+        studentButton.layer.cornerRadius = 10
+        TeachersButton.layer.cornerRadius = 10
     }
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
