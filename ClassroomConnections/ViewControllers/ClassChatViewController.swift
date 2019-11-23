eaf1c9565b50712312c0dd26385aa583997b42ff
RickyYWang <ricky.wyp@gmail.com>
2019-11-23 15:27:42 -0800

    classchatview

--- ClassroomConnections/ViewControllers/ClassChatViewController.swift
+++ ClassroomConnections/ViewControllers/ClassChatViewController.swift
@@ -11,7 +11,33 @@ import UIKit
 class ClassChatViewController: UIViewController {
     
     
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "buh!"),
+        Message(sender: "2@3.COM", body: "BOOH")
+    ]
+    
     override func viewDidLoad() {
+        tableView.dataSource = self
+        tableView.delegate= self
         
     }
+}
+extension ClassChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    
+}
+extension ClassChatViewController: UITableViewDelegate {
+    
 }
---
+++ ClassroomConnections/ViewControllers/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  ClassroomConnections
+//
+//  Created by Ricky Wang on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var sender : String
+    var body : String
+}
