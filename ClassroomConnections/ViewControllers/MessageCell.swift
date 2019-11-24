774b617166dc0b6cfd4b387904fa5bd6e808cdca
RickyYWang <ricky.wyp@gmail.com>
2019-11-23 16:12:35 -0800

    fefefe

---
+++ ClassroomConnections/ViewControllers/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  ClassroomConnections
+//
+//  Created by Ricky Wang on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
