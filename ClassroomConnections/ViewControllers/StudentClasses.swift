b4307429dad8400dd6073ff49635bb8dac47a13d
Michael Peng <mpeng893@student.fuhsd.org>
2019-11-23 22:15:40 -0800

    asdf

--- ClassroomConnections/ViewControllers/StudentClasses.swift
+++ ClassroomConnections/ViewControllers/StudentClasses.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class StudentClasses: UITableViewCell {
+class StudentClasses: UIViewController {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
+    
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    /*
+    // MARK: - Navigation
 
-        // Configure the view for the selected state
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
