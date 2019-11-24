9bf6b3a55e85df91f0034e2ed62acc9bef3cc923
Gavin Wong <gavinmwong@gmail.com>
2019-11-23 19:18:11 -0800

    classes

---
+++ Class.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  randomstuff
+//
+//  Created by Gavin Wong on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import Foundation
+
+struct Class {
+    let classTitle : String
+    let teacher : String
+}
---
+++ ClassroomConnections/ViewControllers/Classes.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  randomstuff
+//
+//  Created by Gavin Wong on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import UIKit
+
+class Classes: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    var textField = UITextField()
+    var classes : [Class] = [
+        Class(classTitle: "APLAC", teacher: "Seike"),
+        Class(classTitle: "AP Minecraft" , teacher: "Your mom")
+    ]
+    
+//    var colors : [UIColor] = [UIColor.brown, UIColor.green]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView.register(UINib(nibName: "AddClass", bundle: nil), forCellReuseIdentifier: "Cell")
+        
+    }
+
+
+}
+
+extension Classes: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return classes.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.row == classes.count) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! AddClass
+            return cell
+
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+            cell.textLabel?.text = classes[indexPath.row].classTitle
+            cell.detailTextLabel?.text = classes[indexPath.row].teacher
+//            cell.backgroundColor = colors[indexPath.row]
+            return cell
+        }
+        
+    }
+    
+}
+
+extension Classes: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if (indexPath.row == classes.count) {
+            let alert = UIAlertController(title: "Enter code to join class", message: "", preferredStyle: .alert)
+            let doneButton = UIAlertAction(title: "Done", style: .default) { (action) in
+                print(self.textField.text!)
+                self.classes.append(Class(classTitle: "AP CSA", teacher: "Fulk"))
+                self.tableView.reloadData()
+            }
+            
+            alert.addAction(doneButton)
+            alert.addTextField { (alertTextField) in
+                alertTextField.placeholder = "Class code"
+                self.textField = alertTextField
+                
+            }
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        print(indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let action = UIContextualAction(style: .normal, title: title,
+          handler: { (action, view, completionHandler) in
+          // Update data source when user taps action
+            self.classes.remove(at: indexPath.row)
+            self.tableView.reloadData()
+          completionHandler(true)
+        })
+        action.backgroundColor = UIColor.red
+        action.title = "Delete"
+        let configuration = UISwipeActionsConfiguration(actions: [action])
+        configuration.performsFirstActionWithFullSwipe = false
+        return configuration
+    }
+//
+//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+//
+//    }
+}
+
