3706a11e300e178e50f469e60673a76e2c864149
Michael Peng <mpeng893@student.fuhsd.org>
2019-11-23 16:49:35 -0800

    asdf

--- ClassroomConnections/ViewControllers/LoginViewController.swift
+++ ClassroomConnections/ViewControllers/LoginViewController.swift
@@ -7,11 +7,43 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
     
+//    var ref : DatabaseReference!
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     override func viewDidLoad() {
         
+    }
+    @IBAction func loginPressed(_ sender: Any) {
+        SVProgressHUD.show()
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            if (error == nil) {
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToMainMenu", sender: self)
+            } else {
+                
+                SVProgressHUD.dismiss()
+                
+                let alert = UIAlertController(title: "Login Error", message: "Incorrect username or password", preferredStyle: .alert)
+                let forgotPassword = UIAlertAction(title: "Forgot Password?", style: .default, handler: { (UIAlertAction) in
+                    //do the forgot password shit
+                })
+                
+                let cancel = UIAlertAction(title: "Cancel", style: .default, handler: { (UIAlertAction) in
+                    //do nothing
+                })
+                
+                alert.addAction(forgotPassword)
+                alert.addAction(cancel)
+                self.present(alert, animated: true, completion: nil)
+                print("error with logging in: ", error!)
+            }
+        }
     }
 }
