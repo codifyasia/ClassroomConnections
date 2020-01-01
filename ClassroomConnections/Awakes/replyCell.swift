08f613f544bb4558860d826f67124217b4eb602a
Gavin Wong <gavinmwong@gmail.com>
2019-12-31 17:03:59 -0800

    changed reply cell

---
+++ ClassroomConnections/Awakes/replyCell.swift
@@ -0,0 +1,36 @@
+//
+//  questionCell.swift
+//  ClassroomConnections
+//
+//  Created by Michael Peng on 12/22/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import UIKit
+
+class replyCell: UITableViewCell {
+
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var senderName: UILabel!
+    @IBOutlet weak var messageBubble: UIView!
+    //        @IBOutlet weak var upvoteImage: UIImageView!
+//        @IBOutlet weak var upvoteAmount: UILabel!
+//        @IBOutlet weak var label: UILabel!
+//    //
+//        @IBOutlet weak var rightImage: UIImageView!
+//        //    @IBOutlet weak var rightImage: UIImageView!
+//        @IBOutlet weak var senderName: UILabel!
+//        @IBOutlet weak var messageBubble: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
