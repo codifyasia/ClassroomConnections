1e669c0699838b2840014eb32dec2091ca57a166
Michael Peng <michael123.peng@gmail.com>
2020-07-01 00:57:56 -0700

    asdf

---
+++ ClassroomConnections/ViewControllers/ViewStudentsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewStudentsViewController.swift
+//  ClassroomConnections
+//
+//  Created by Michael Peng on 7/1/20.
+//  Copyright © 2020 CodifyAsia. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewStudentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ref = Database.database().reference()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
